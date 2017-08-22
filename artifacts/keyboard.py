bd65c6956c9f5d2eea81c0bf8c00fe176a7d455d
Joseph Chilcote <chilcote+github@gmail.com>
2017-08-22 10:26:26 -0700

    adding keyboard artifact (hat tip allister in slack)

---
+++ artifacts/keyboard.py
@@ -0,0 +1,27 @@
+# ref: allister in slack
+import plistlib
+import subprocess
+
+factoid = 'keyboard'
+
+def fact():
+    '''Returns the keyboard localization'''
+    result = None
+
+    try:
+        proc = subprocess.Popen(['/usr/sbin/ioreg',
+                                 '-rln',
+                                 'AppleHIDKeyboardEventDriverV2',
+                                 '-a'],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, _ = proc.communicate()
+    except (IOError, OSError):
+        result = 'Unknown'
+
+    if output:
+        result = plistlib.readPlistFromString(output)[0].get('KeyboardLanguage')
+
+    return {factoid: result.strip()}
+
+if __name__ == '__main__':
+    print '<result>%s</result>' % fact()[factoid]
