998a2118ad77bae26daf1d48dfb3b788722d3592
Joseph Chilcote <chilcote+github@gmail.com>
2017-08-29 11:59:20 -0700

    fixes #2 to not require an external_fact call

--- artifacts/mac_address.py
+++ artifacts/mac_address.py
@@ -6,27 +6,27 @@ from SystemConfiguration import SCDynamicStoreCreate, SCDynamicStoreCopyValue
 
 factoid = 'mac_address'
 
-def get_external_fact(category):
-    '''Returns info from another module in this project'''
-    d = {}
-    filename = os.path.join(os.path.dirname(__file__), category + '.py')
-    if os.path.exists(filename):
-        try:
-            module = imp.load_source(category, filename)
-            d = module.fact()
-        except (ImportError, AttributeError), err:
-            print >> sys.stderr, 'Error %s in file %s' % (err, filename)
-    return d[category]
+def primary_interface():
+    '''Returns the primary interface of this Mac'''
+    primary_interface = None
+    net_config = SCDynamicStoreCreate(None, "net", None, None)
+    try:
+        states = SCDynamicStoreCopyValue(net_config, "State:/Network/Global/IPv4")
+        primary_interface = states['PrimaryInterface']
+    except TypeError:
+        pass
+
+    return primary_interface
 
 def fact():
     '''Returns the mac address of this Mac'''
     mac_address = None
-    primary_interface = get_external_fact('primary_interface')
-    if primary_interface:
+    iface = primary_interface()
+    if iface:
         try:
             proc = subprocess.Popen(['/usr/sbin/networksetup',
                                     '-getmacaddress',
-                                    primary_interface],
+                                    iface],
                                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, _ = proc.communicate()
             mac_address = stdout.strip().split(' ')[2]
