52783bb2de77517e002ef770a0550cb65b053aa1
Matthew Warren <bmwarren@unca.edu>
2017-09-16 12:57:10 -0400

    add: drive_medium artifact

---
+++ artifacts/drive_medium.py
@@ -0,0 +1,38 @@
+import subprocess
+import plistlib
+
+factoid = "drive_medium"
+
+def fact():
+    '''
+    Returns the medium type for the boot drive of this Mac
+
+    Values include 'fusion', 'rotational', 'ssd' or 'None'
+    '''
+    result = 'None'
+    try:
+        # Check for Fusion drive
+        proc = subprocess.Popen(['/usr/sbin/diskutil', 'cs', 'list'],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, _ = proc.communicate()
+        if 'Fusion' in stdout:
+            result = 'fusion'
+        else:
+            # Determine rotational vs ssd
+            proc = subprocess.Popen(['/usr/sbin/system_profiler', '-xml',
+                                        'SPStorageDataType'],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+            stdout, _ = proc.communicate()
+            xml = plistlib.readPlistFromString(stdout)
+            drives = xml[0]['_items']
+            for drive in drives:
+                if drive['mount_point'] == '/':
+                    result = drive['physical_drive']['medium_type']
+    except (IOError, OSError):
+        pass
+    
+    return {factoid: result}
+
+if __name__ == '__main__':
+    print '<result>%s</result>' % fact()[factoid]
