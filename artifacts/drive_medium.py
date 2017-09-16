81873465606595f32d1cec231d3e8263c7667626
Joseph Chilcote <chilcote+github@gmail.com>
2017-09-16 13:47:59 -0700

    modified drive_medium

--- artifacts/drive_medium.py
+++ artifacts/drive_medium.py
@@ -1,7 +1,7 @@
 import subprocess
 import plistlib
 
-factoid = "drive_medium"
+factoid = 'drive_medium'
 
 def fact():
     '''
@@ -15,27 +15,28 @@ def fact():
         proc = subprocess.Popen(['/usr/sbin/diskutil', 'cs', 'list'],
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, _ = proc.communicate()
-        if 'Fusion' in stdout:
+        if 'fusion' in stdout.lower():
             result = 'fusion'
         else:
             # Determine rotational vs ssd
             proc = subprocess.Popen(['/usr/sbin/system_profiler', '-xml',
-                                        'SPStorageDataType'],
+                                     'SPStorageDataType'],
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE)
             stdout, _ = proc.communicate()
-            xml = plistlib.readPlistFromString(stdout)
-            drives = xml[0]['_items']
+            drives = plistlib.readPlistFromString(stdout)[0]['_items']
             for drive in drives:
                 if drive['mount_point'] == '/':
                     # Check for CoreStorage physical volume
                     try:
-                        result = drive[0]['com.apple.corestorage.pv']['medium_type']
+                        result = drive['com.apple.corestorage.pv'][0]['medium_type']
                     except KeyError:
                         result = drive['physical_drive']['medium_type']
+                    except KeyError:
+                        pass
     except (IOError, OSError):
         pass
-    
+
     return {factoid: result}
 
 if __name__ == '__main__':
