0c32070cb4c87af0533683ca5efca8603070e745
Joseph Chilcote <chilcote+github@gmail.com>
2017-09-17 11:13:57 -0700

    updated drive_medium

--- artifacts/drive_medium.py
+++ artifacts/drive_medium.py
@@ -4,38 +4,27 @@ import plistlib
 factoid = 'drive_medium'
 
 def fact():
-    '''
-    Returns the medium type for the boot drive of this Mac
-
-    Values include 'fusion', 'rotational', 'ssd' or 'None'
-    '''
+    '''Returns the boot drive medium'''
     result = 'None'
+
     try:
-        # Check for Fusion drive
-        proc = subprocess.Popen(['/usr/sbin/diskutil', 'cs', 'list'],
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(
+                ['/usr/sbin/diskutil', 'info', '-plist', '/'],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+                )
         stdout, _ = proc.communicate()
-        if 'fusion' in stdout.lower():
-            result = 'fusion'
-        else:
-            # Determine rotational vs ssd
-            proc = subprocess.Popen(['/usr/sbin/system_profiler', '-xml',
-                                     'SPStorageDataType'],
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
-            stdout, _ = proc.communicate()
-            drives = plistlib.readPlistFromString(stdout)[0]['_items']
-            for drive in drives:
-                if drive['mount_point'] == '/':
-                    # Check for CoreStorage physical volume
-                    try:
-                        result = drive['com.apple.corestorage.pv'][0]['medium_type']
-                    except KeyError:
-                        result = drive['physical_drive']['medium_type']
-                    except KeyError:
-                        pass
     except (IOError, OSError):
-        pass
+        stdout = None
+
+    if stdout:
+        d = plistlib.readPlistFromString(stdout.strip())
+        if d.get('CoreStorageCompositeDisk', False):
+            result = 'fusion'
+        elif d.get('RAIDMaster', False):
+            result = 'raid'
+        else:
+            result = 'ssd' if d.get('SolidState', False) else 'rotational'
 
     return {factoid: result}
 
