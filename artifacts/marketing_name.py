8ea87b466a602ea4abd330bc31268342d6ba3be9
Joseph Chilcote <chilcote+github@gmail.com>
2017-11-11 14:23:14 -0800

    adding marketing name fact

---
+++ artifacts/marketing_name.py
@@ -0,0 +1,32 @@
+# ref: http://scriptingosx.com/2017/11/get-the-marketing-name-for-a-mac/
+import sys
+import plistlib
+import subprocess
+from Cocoa import NSBundle
+
+factoid = 'marketing_name'
+
+def fact():
+    '''Returns the marketing name'''
+    result = 'None'
+    model = subprocess.check_output(
+            ['/usr/sbin/sysctl', '-n', 'hw.model']
+            ).strip()
+
+    serverInfoBundle = NSBundle.bundleWithPath_(
+            '/System/Library/PrivateFrameworks/ServerInformation.framework/'
+            )
+    sysinfofile = serverInfoBundle.URLForResource_withExtension_subdirectory_(
+            'SIMachineAttributes', 'plist', ''
+            )
+
+    d = plistlib.readPlist(sysinfofile.path())
+
+    if d.get(model, False):
+        result = d[model]['_LOCALIZABLE_']['marketingModel']
+
+    return {factoid: result.strip()}
+
+if __name__ == '__main__':
+    print '<result>%s</result>' % fact()[factoid]
+
