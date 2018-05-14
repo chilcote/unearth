8f647803797e477753349372838b548964bab6a6
Matthew Warren <3910+haircut@users.noreply.github.com>
2018-05-14 16:19:28 -0400

    Add AFP and SMB file sharing statuses

---
+++ artifacts/file_sharing_status_afp.py
@@ -0,0 +1,20 @@
+import subprocess
+
+factoid = 'file_sharing_status_afp'
+
+def fact():
+    '''Returns the status of AFP file sharing'''
+    try:
+        status = False
+        proc = subprocess.Popen(['/bin/launchctl', 'list'],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result, _ = proc.communicate()
+        if 'com.apple.AppleFileServer' in result:
+            status = True
+    except (IOError, OSError):
+        status = 'Unknown'
+
+    return {factoid: status}
+
+if __name__ == '__main__':
+    print '<result>%s</result>' % fact()[factoid]
