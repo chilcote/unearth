52e798bafe3858527d0ff5824ed56282cf43057b
Joseph Chilcote <chilcote+github@gmail.com>
2019-01-06 15:59:20 -0800

    fix active_directory_last_dc

--- artifacts/active_directory_last_dc.py
+++ artifacts/active_directory_last_dc.py
@@ -8,20 +8,21 @@ def fact():
     '''Returns the last used domain controller'''
 
     result = 'None'
+    domain = None
     plist = None
 
     try:
         plist = glob.glob('/Library/Preferences/OpenDirectory/DynamicData/Active Directory/*.plist')[0]
     except IndexError:
         pass
-
     if plist and os.path.exists(plist):
         last_used_servers = CFPreferencesCopyAppValue('last used servers', plist)
         for k in last_used_servers.keys():
             if 'Global Catalog' not in k:
                 domain = k
                 break
-        result = last_used_servers[domain]['host']
+        if domain:
+            result = last_used_servers[domain]['host']
 
     return {factoid: result}
 
