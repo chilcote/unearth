4768e60c0f91a4382f5289a0c209d076828af5c7
AP <aporlebeke@gmail.com>
2019-01-06 16:54:43 -0500

    Add cpu artifacts
    - CPU speed
    - CPU type
    - CPU cores
    - CPU L2 cache size
    - CPU L3 cache size

---
+++ artifacts/cpu_l2_cache.py
@@ -0,0 +1,28 @@
+import subprocess
+import plistlib
+
+factoid = 'cpu_l2_cache'
+
+def fact():
+    '''Returns the CPU L2 cache'''
+
+    result = 'None'
+
+    try:
+        proc = subprocess.Popen(
+                ['/usr/sbin/system_profiler', 'SPHardwareDataType', '-xml'],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+                )
+        stdout, _ = proc.communicate()
+    except (IOError, OSError):
+        stdout = None
+
+    if stdout:
+        d = plistlib.readPlistFromString(stdout.strip())
+        result = d[0]['_items'][0]['l2_cache_core']
+
+    return {factoid: result}
+
+if __name__ == '__main__':
+    print '<result>%s</result>' % fact()[factoid]
